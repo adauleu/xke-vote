dd929cf7074af13c7a76c091887f11025fce291a
Alban Dauleu <adauleu@xebia.fr>
2017-04-10 12:46:33 +0200

    XKE Avril

--- server/conf/slots.js
+++ server/conf/slots.js
@@ -1,104 +1,91 @@
 module.exports = {
-  am: [
-    {
-      period: '10h00-12h00',
-      talks: [
-        {
-          id: 1,
-          text: 'In-Memory Computing avec Apache Ignite',
-          fondation: 'Back',
-          attendees: 0,
-        },
-        {
-          id: 2,
-          text: 'VueJS',
-          fondation: 'Front',
-          attendees: 0,
-        },
-        {
-          id: 3,
-          text: 'Transfo Agile chez EDF + Devops 101',
-          fondation: 'Divers',
-          attendees: 0,
-        },
-        {
-          id: 4,
-          text: 'Automatiser mon infrastructure + AWS - Introduction aux VPC & IAM',
-          fondation: 'Divers',
-          attendees: 0,
-        },
-      ],
+    am: [{
+        period: '11h00-12h00',
+        talks: [
+            {
+                id: 1,
+                text: 'AWS - RDS',
+                fondation: 'Cloud',
+                attendees: 0
+            },
+            {
+                id: 2,
+                text: 'Présentation de Apache Kudu',
+                fondation: 'Data',
+                attendees: 0
+            },
+            {
+                id: 3,
+                text: 'REX Comment on a monté un mini-ingenico en un mois / Team ingenico',
+                fondation: 'Divers',
+                attendees: 0
+            },
+            {
+                id: 4,
+                text: 'Bootcamp - Accueil Nouveaux Arrivants',
+                fondation: 'Divers',
+                attendees: 13
+            }
+        ]
     }],
-  pm: [
-    {
-      period: '14h30-16h30',
-      talks: [
+    pm: [
         {
-          id: 1,
-          text: 'Android Things & SwiftyGPIO',
-          fondation: 'Mobile',
-          attendees: 0,
+            period: '14h00-16h00',
+            talks: [
+                {
+                    id: 1,
+                    text: 'Search and Hbase. Indexer ses données au fil de l\'eau',
+                    fondation: 'Data',
+                    attendees: 0
+                },
+                {
+                    id: 2,
+                    text: 'Transformer sa banale application web en PWA du futur',
+                    fondation: 'Mobile',
+                    attendees: 0
+                },
+                {
+                    id: 3,
+                    text: "Algorithmes du quotidien",
+                    fondation: 'Divers',
+                    attendees: 0
+                },
+                {
+                    id: 4,
+                    text: 'AWS : StepFunctions workflow de lambda + REX : Devops @ Photobox',
+                    fondation: 'Cloud',
+                    attendees: 0
+                }
+            ]
         },
         {
-          id: 2,
-          text: 'Akka stream for Image processing + Akka et microservices',
-          fondation: 'Back',
-          attendees: 0,
-        },
-        {
-          id: 3,
-          text: 'Machine Learning + DynamoDB',
-          fondation: 'Divers',
-          attendees: 0,
-        },
-        {
-          id: 4,
-          text: 'Rally Test Game + Oubliez les Frameworks, pensez Patterns',
-          fondation: 'Agile',
-          attendees: 0,
-        },
-        {
-          id: 5,
-          text: 'LaPoste OpenAPI (API Management) + Type Wars',
-          fondation: 'Divers',
-          attendees: 0,
-        },
-      ],
-    },
-    {
-      period: '16h30-18h30',
-      talks: [
-        {
-          id: 6,
-          text: 'Git',
-          fondation: 'Divers',
-          attendees: 0,
-        },
-        {
-          id: 7,
-          text: 'AWS Lambda',
-          fondation: 'Cloud',
-          attendees: 0,
-        },
-        {
-          id: 8,
-          text: 'Open Space Agile + Gamify your todo list',
-          fondation: 'Agile',
-          attendees: 0,
-        },
-        {
-          id: 9,
-          text: 'Algorithmes du quotidien',
-          fondation: 'Divers',
-          attendees: 0,
-        },
-        {
-          id: 10,
-          text: 'Simple typeclass derivation + BigData for dummies',
-          fondation: 'Divers',
-          attendees: 0,
-        },
-      ],
-    },
-  ],
+            period: '16h30-18h30',
+            talks: [
+                {
+                    id: 5,
+                    text: 'SMACK : Embrassez le Fast Data',
+                    fondation: 'Data',
+                    attendees: 0
+                },
+                {
+                    id: 6,
+                    text: 'Swift Basics',
+                    fondation: 'Mobile',
+                    attendees: 0
+                },
+                {
+                    id: 7,
+                    text: ' Open Space Agile + XScale',
+                    fondation: 'Agile',
+                    attendees: 0
+                },
+                {
+                    id: 8,
+                    text: 'PhantomJS + Spark ML',
+                    fondation: 'Divers',
+                    attendees: 0
+                }
+            ]
+        }
+    ]
 };
